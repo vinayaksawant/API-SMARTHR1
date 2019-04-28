b76a0f4ebc621aa9482c25455947045d3625f3c0
vinayak.sawant <vinayak.sawant@cvshealth.com>
2019-04-28 18:47:33 -0500

    Employers Entity Controller

---
+++ APISMARTHR1/Controllers/EmployersController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APISMARTHR1.Entities;
+
+namespace APISMARTHR1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployersController : ControllerBase
+    {
+        private readonly EmployerContext _context;
+
+        public EmployersController(EmployerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Employers
+        [HttpGet]
+        public IEnumerable<Employer> GetEmployer()
+        {
+            return _context.Employer;
+        }
+
+        // GET: api/Employers/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployer([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var employer = await _context.Employer.FindAsync(id);
+
+            if (employer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employer);
+        }
+
+        // PUT: api/Employers/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployer([FromRoute] int id, [FromBody] Employer employer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != employer.EmployerID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Employers
+        [HttpPost]
+        public async Task<IActionResult> PostEmployer([FromBody] Employer employer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Employer.Add(employer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmployer", new { id = employer.EmployerID }, employer);
+        }
+
+        // DELETE: api/Employers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployer([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var employer = await _context.Employer.FindAsync(id);
+            if (employer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employer.Remove(employer);
+            await _context.SaveChangesAsync();
+
+            return Ok(employer);
+        }
+
+        private bool EmployerExists(int id)
+        {
+            return _context.Employer.Any(e => e.EmployerID == id);
+        }
+    }
+}
